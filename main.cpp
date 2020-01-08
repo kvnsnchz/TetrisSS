7b993588b67f043d40b5766847b301b9e58eb6de
Sielskyi Yevhenii <yevhenii.sielskyi@ensta-paris.fr>
2020-01-08 22:21:57 +0100

    Started main menu

--- main.cpp
+++ main.cpp
@@ -1,30 +1,51 @@
 #include <unistd.h>
 #include "board.hpp"
 
-void game() {
-    // get user's resolution:
-    Vector2f resolution(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
-
-    // create the game window
-    // which size depends on user's screen resolution:
-    RenderWindow window(VideoMode(min(41.0f * 14 + 20.0f, resolution.x / 2), min(41.0f * 20 + 9.0f, resolution.y - 100.0f)), "TetrisSS 1.0");
-    // create the game board: 
-    Board *game_board = new Board(window);
-
-    // set the background image:
-    Texture texture;
+void main_menu(RenderWindow& window, const Sprite& background) {
+    Font font;
     try {
-        if (!texture.loadFromFile("background/bg2.jpg")) 
+        if (!font.loadFromFile("amatic/Amatic-Bold.ttf")) 
             throw 0;
     } catch (int e) {
         if (e == 0)
-            cout << "Sorry, background image not found!" << endl;
+            cout << "Sorry, font file not found!" << endl;
     }
 
-    // create a background itself:
-    Sprite background;
-    background.setTexture(texture);
-    background.setColor(Color(255, 255, 255, 200));
+    Text new_game;
+    new_game.setFont(font);
+    new_game.setString("New Game");
+    new_game.setPosition(window.getSize().x / 2 - 50.0f, 25.0f);
+    new_game.setCharacterSize(50); 
+    new_game.setFillColor(Color(242, 0, 0, 255));
+    new_game.setStyle(Text::Bold);
+
+    while (window.isOpen()) {
+        Event event;
+
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                // close window:  
+                case Event::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        window.clear();
+        window.draw(background);
+        window.draw(new_game);
+        window.display();
+    }    
+}
+
+void game(RenderWindow& window, const Vector2f& resolution, const Sprite& background) {
+    // change window's size:
+    window.setSize(Vector2u(min(41.0f * 14 + 20.0f, resolution.x / 2), min(41.0f * 20 + 9.0f, resolution.y - 100.0f)));
+
+    // create the game board: 
+    Board *game_board = new Board(window);
 
     while (window.isOpen()) {
         Event event;
@@ -106,16 +127,38 @@ void game() {
             game_board->set_next_figure(game_board->create_figure());
         }
 
-        usleep(200000);
+        usleep(300000);
     }
 
     delete game_board;
 }
 
 int main(int argc, char const *argv[]) {
+    // get user's resolution:
     Vector2f resolution(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
 
-    game();
+    // create the game window
+    // which size depends on user's screen resolution:
+    RenderWindow window(VideoMode(min(41.0f * 14 + 20.0f, resolution.x / 2), min(41.0f * 20 + 9.0f, resolution.y - 100.0f)), "TetrisSS 1.0");
+    
+    // set the background image:
+    Texture texture;
+    try {
+        if (!texture.loadFromFile("background/bg2.jpg")) 
+            throw 0;
+    } catch (int e) {
+        if (e == 0)
+            cout << "Sorry, background image not found!" << endl;
+    }
+
+    // create a background itself:
+    Sprite background;
+    background.setTexture(texture);
+    background.setColor(Color(255, 255, 255, 200));
+
+    // main_menu(window, background);
+
+    game(window, resolution, background);
 
     return 0;
 }
