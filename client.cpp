02a8e66d5b83404d51bb0dcd1ab92144508c2fef
kvnsnchz <kvn9616@gmail.com>
2020-01-26 05:10:56 +0100

    Adding server connection

--- client.cpp
+++ client.cpp
@@ -1,32 +1,128 @@
 #include "client.hpp"
 
+vector<server_t> Client::get_servers() const {
+    return servers;
+}
+
 void Client::search_servers(){
-    thread thread_socket([&] () {
-        UdpSocket socket;
+    UdpSocket socket;
 
-        if (socket.bind(CLIENT_PORT) != sf::Socket::Done)
-        {
-            cout << "Client: Connection error" << endl;
-        }
+    if (socket.bind(CLIENT_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Connection error" << endl;
+        return;
+    }
+    
+    Packet packet_send;
+    packet_send << SERVER_INFO_REQUEST;
+    
+    if (socket.send(packet_send, IpAddress::Broadcast, SERVER_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Send error" << endl;
+        return;
+    }
 
-        vector<string> data;
-        data.emplace_back(REQUEST_CONNECTION);
-
-        if (socket.send(data.data(), data.size(), IpAddress::Broadcast, SERVER_PORT) != sf::Socket::Done)
-        {
-            cout << "Client: Send error" << endl;
-        }
-
-    /* vector<string>(2).swap(data);
+    chrono::time_point<chrono::system_clock> start = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds;
+    socket.setBlocking(false);
+    
+    do{
+        Packet packet_recv;
         IpAddress sender;
-        size_t received;
-
-        if (socket.receive(data.data(), 2, received, sender, port) != sf::Socket::Done)
+        unsigned short port;
+        
+        if (socket.receive(packet_recv, sender, port) == Socket::Done)
         {
-            cout << "Client: Receive error" << endl;
+            string _datatype;
+            
+            packet_recv >> _datatype;
+            
+            if(!_datatype.compare(SERVER_INFO_RESPONSE)){
+                servers.emplace_back(server_t());
+                servers.back().address = sender;
+                packet_recv >> servers.back().clients_quantity;
+                cout << "Server: address = " << servers.back().address << " clients_quantity = " << servers.back().clients_quantity << endl;
+            }
         }
-        std::cout << "Received " << received << " bytes from " << sender << " on port " << port << std::endl;
-        */
-    });
-    thread_socket.join();
+        elapsed_seconds = chrono::system_clock::now() - start;
+    } while(elapsed_seconds.count() <= MAX_SEARCH_TIME);
+    
+}
+
+
+bool Client::connect_server(const unsigned pos){
+    if(pos > servers.size())
+        return false;
+    
+    UdpSocket socket;
+
+    if (socket.bind(CLIENT_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Connection error" << endl;
+        return false;
+    }
+    
+    Packet packet_send;
+    packet_send << SERVER_CONN_REQUEST;
+    
+    if (socket.send(packet_send, servers[pos].address, SERVER_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Send error" << endl;
+        return false;
+    }
+
+    chrono::time_point<chrono::system_clock> start = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds;
+    socket.setBlocking(false);
+    
+    do{
+        Packet packet_recv;
+        IpAddress sender;
+        unsigned short port;
+        
+        if (socket.receive(packet_recv, sender, port) == Socket::Done)
+        {
+            if(sender == servers[pos].address){
+                string _datatype;
+                
+                packet_recv >> _datatype;
+                
+                if(!_datatype.compare(SERVER_CONN_RESPONSE_ERROR)){
+                    servers.erase(servers.begin() + pos);
+                    return false;
+                }
+                if(!_datatype.compare(SERVER_CONN_RESPONSE_SUCCESS)){
+                    server_address = servers[pos].address;
+                    cout << "Connected to the Server " << server_address << endl;
+                    return true;
+                }
+            }
+        }
+        elapsed_seconds = chrono::system_clock::now() - start;
+    } while(elapsed_seconds.count() <= MAX_CONNECTION_TIME);
+
+    return false;
+}
+
+bool Client::disconnect_server(){
+    UdpSocket socket;
+
+    if (socket.bind(CLIENT_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Connection error" << endl;
+        return false;
+    }
+    
+    Packet packet_send;
+    packet_send << SERVER_DISCONNECTION;
+    
+    if (socket.send(packet_send, server_address, SERVER_PORT) != sf::Socket::Done)
+    {
+        cout << "Client: Send error" << endl;
+        return false;
+    }
+
+    server_address = IpAddress::None;
+    cout << "Disconnected to the Server " << server_address << endl;
+    return true;
 }
--- server.cpp
+++ server.cpp
@@ -1,25 +1,64 @@
 #include "server.hpp"
 
 void Server::search_clients() {
-    thread thread_socket([&] () {
-        UdpSocket socket;
-        socket.setBlocking(false);
-        
-        if (socket.bind(SERVER_PORT) != Socket::Done)
+    UdpSocket socket;
+    socket.setBlocking(false);
+    
+    if (socket.bind(SERVER_PORT) != Socket::Done)
+    {
+        cout << "Server: Connection error" << endl;
+        return;
+    }
+
+    Packet packet_recv;;
+    IpAddress sender;
+    unsigned short port;
+
+    while(true){
+        if (socket.receive(packet_recv, sender, port) == Socket::Done)
         {
-            cout << "Server: Connection error" << endl;
-        }
+            string _datatype;
+            Packet packet_send;
 
-        vector<string> data(1);
-        IpAddress sender;
-        size_t received;
+            packet_recv >> _datatype;
+            
+            if(!_datatype.compare(SERVER_INFO_REQUEST)){
+                packet_send << SERVER_INFO_RESPONSE;
+                packet_send << (Uint32)clients_address.size();
+                
+                if (socket.send(packet_send, sender, CLIENT_PORT) != sf::Socket::Done)
+                    cout << "Server: Send error" << endl;
 
-        while(true){
-            if (socket.receive(data.data(), 1, received, sender, port) == Socket::Done)
-            {
-            std::cout << "Received " << received << " bytes from " << sender << " on port " << port << std::endl;
+                cout << "Client " << _datatype << endl;
             }
+            else{
+                if(!_datatype.compare(SERVER_CONN_REQUEST)){
+                    if(clients_address.size() == MAX_CLIENTS){
+                        packet_send << SERVER_CONN_RESPONSE_ERROR;
+                    }
+                    else{
+                        if(find(clients_address.begin(), clients_address.end(), sender) == clients_address.end())
+                            clients_address.emplace_back(sender);
+                            
+                        packet_send << SERVER_CONN_RESPONSE_SUCCESS;
+                    }
+
+                    if (socket.send(packet_send, sender, CLIENT_PORT) != sf::Socket::Done)
+                        cout << "Server: Send error" << endl;
+                    
+                    cout << "New client " << sender << endl;
+                }
+                else{
+                    if(!_datatype.compare(SERVER_DISCONNECTION)){
+                        vector<IpAddress>::iterator it = find(clients_address.begin(), clients_address.end(), sender);
+                        if(it != clients_address.end()){
+                            clients_address.erase(it);
+                            cout << "Delete client " << sender << endl;
+                        }
+                    }
+                }
+            }
+            std::cout << "Received bytes from " << sender << " on port " << port << std::endl;
         }
-    });
-    thread_socket.join();
+    }
 }
